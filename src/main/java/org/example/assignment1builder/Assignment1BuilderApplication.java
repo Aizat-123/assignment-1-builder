84c4abf902dc07723e0b1e8675040bf4c46c6ecf
Aizat Kabdyrakmanova <aizatkabdyrakmanova@gmail.com>
2026-09-16 00:16:35 +0500

    Initialize assignment-1-builder project with SmartHome, Address and Main classes; README and report files

---
+++ src/main/java/org/example/assignment1builder/Assignment1BuilderApplication.java
@@ -0,0 +1,13 @@
+package org.example.assignment1builder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Assignment1BuilderApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Assignment1BuilderApplication.class, args);
+    }
+
+}
---
+++ src/test/java/org/example/assignment1builder/Assignment1BuilderApplicationTests.java
@@ -0,0 +1,13 @@
+package org.example.assignment1builder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Assignment1BuilderApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
